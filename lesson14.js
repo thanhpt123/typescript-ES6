4a59666890d2149f37a8527c163981db3cdef0e1
Thanhpt <trungthanhplhd@gmail.com>
2018-05-22 10:57:56 +0700

    commit test git

---
+++ lesson14.js
@@ -0,0 +1,6 @@
+// commit git tester
+var thanos = function (value) {
+    message = "thanos c\u00E2n team " + value;
+    console.log(message);
+};
+thanos("15 GG");
---
+++ lesson14.ts
@@ -0,0 +1,6 @@
+// commit git tester
+var thanos = function(value:string){
+    message = `thanos cân team ${value}`;
+    console.log(message);
+}
+thanos("15 GG");
