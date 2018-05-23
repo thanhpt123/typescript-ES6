69d761dbf244e445deb95dccd220dd65dc2530db
Thanhpt <trungthanhplhd@gmail.com>
2018-05-23 08:51:07 +0700

    vong lap for

---
+++ lesson15.js
@@ -0,0 +1,13 @@
+// vòng lặp for test
+var colorNames = ["Blue", "Green", "Pink", "Puple"];
+for (var i in colorNames) {
+    console.log(colorNames[i]);
+}
+for (var _i = 0, colorNames_1 = colorNames; _i < colorNames_1.length; _i++) {
+    var item = colorNames_1[_i];
+    console.log(item);
+}
+var nameLesson15 = "ThanhPT";
+for (var c of nameLesson15) {
+    console.log(c);
+}
---
+++ lesson15.ts
@@ -0,0 +1,12 @@
+// vòng lặp for test
+var colorNames = ["Blue", "Green","Pink", "Puple"];
+for(let i in colorNames){
+    console.log(colorNames[i]);
+}
+for(let item of colorNames){
+    console.log(item);
+}
+let nameLesson15:any = "ThanhPT";
+for(let c of nameLesson15){
+    console.log(c);
+}
